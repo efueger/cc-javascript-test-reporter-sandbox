eb86cd1da81ce22414ef7201ceb0a001f710c17c
Jonathan Powers <bigblackrobot@gmail.com>
2014-09-28 10:36:52 -0400

    Create bitwise.js

---
+++ src/bitwise.js
@@ -0,0 +1,13 @@
+// helper function for displaing results
+var output = function(operator, result) {
+    document.getElementById(operator).innerHTML = result;
+}
+
+// variables
+var a = 5;
+var b = 13;
+
+
+// ~a - NOT
+// inverts all the bits
+output('not', ~a); // -6
