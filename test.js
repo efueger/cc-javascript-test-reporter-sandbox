a5ba265a11a0a7223fb3a855c011bdcd36e6422c
Jonathan Powers <bigblackrobot@gmail.com>
2014-11-17 08:21:11 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,25 @@
+(function( undefined ) {
+    "use strict";
+
+    function isPrime( number ) {
+        var prime = true, i;
+      
+        if ( isNumber( number ) ) {
+            for ( i = 2; i < number; i++ ) {
+              if ( number % i === 0 ) {
+                  prime = false;
+              } 
+            }
+        } else {
+            prime = false;
+        }
+      
+        return prime;
+    }
+  
+    function isNumber( number ) {
+        return !isNaN( parseFloat( number ) ) && isFinite( number );
+    }
+  
+    console.log( isPrime( 109 ) );
+}());
